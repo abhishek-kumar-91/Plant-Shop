7b3a61c9e5f928d2c71ab2c765ac2b344686faa6
Abhishek <abhishekkumar958830@gmail.com>
2023-05-06 23:00:48 +0530

    Plant

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Planet Paradise 2023 | All Rights Reserved </b></span></div>
+</footer>
+    
